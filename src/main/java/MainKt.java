a2e4053b2dc1a3b980aaddc2246ab4d747398849
Pavel Grigorenko <GrigorenkoPV@niuitmo.ru>
2025-10-14 12:17:17 +0300

    Benchmark

--- src/main/java/MainKt.java
+++ src/main/java/MainKt.java
@@ -7,9 +7,29 @@ public class MainKt {
     public static void main(String[] args) {
         final RandomGraphGenerator generator = new RandomGraphGenerator();
         final Random random = new Random(4);
-        for (int i = 0; i < 1000; i++) {
-            final Graph graph = generator.generateGraph(random, 1_000_000, 1_000_000);
-            graph.parallelBFS(0);
+        for (int i = 0; i < 100; i++) {
+            System.err.println("Warmup " + i);
+            generator.generateGraph(random, 100_000, 1_000_000);
+        }
+        final int[] sizes = {10, 100, 1000, 10_000, 10_000, 50_000, 100_000};
+        for (final int size : sizes) {
+            for (int i = 0; i <= size / 5; i += Math.max(1, size / 50)) {
+                final int numEdges = i * i;
+                if (numEdges < size) {
+                    continue;
+                }
+                if (numEdges > size * (size - 1) / 2) {
+                    break;
+                }
+                System.out.print(size + " " + numEdges + " ");
+                System.out.flush();
+                final Graph graph = generator.generateGraph(random, size, numEdges);
+                final long start = System.nanoTime();
+                graph.parallelBFS(0);
+                final long end = System.nanoTime();
+                final long duration = end - start;
+                System.out.println(duration);
+            }
         }
     }
 }
