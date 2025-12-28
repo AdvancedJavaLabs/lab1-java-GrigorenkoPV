8446f3a5253dbed2f8890e97015734654d1740b4
Pavel Grigorenko <GrigorenkoPV@niuitmo.ru>
2025-12-28 22:16:08 +0300

    Fronts

--- src/main/java/org/itmo/AtomicBooleanArray.java
+++ src/main/java/org/itmo/AtomicBooleanArray.java
@@ -1,5 +1,6 @@
 package org.itmo;
 
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public final class AtomicBooleanArray {
@@ -11,5 +12,13 @@ public final class AtomicBooleanArray {
 
     public boolean getAndSet(int i, boolean value) {
         return inner.getAndSet(i, value ? 1 : 0) != 0;
+    }
+
+    public boolean[] toArray() {
+        final boolean[] result = new boolean[inner.length()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = inner.get(i) != 0;
+        }
+        return result;
     }
 }
